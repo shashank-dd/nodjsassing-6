2848266492f9d5f45964601124404e58f6a0a3ba
shashank-dd <119873380+shashank-dd@users.noreply.github.com>
2022-12-14 11:13:55 +0530

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -21,10 +21,11 @@ router.get("/blog",async(req,res)=>{
         
             
               const user =await Blog.find({topic:search})
-              res.json({
-                  users:users,
-                 user:user 
-              })
+              res.json("fello")
+//               res.json({
+//                   users:users,
+//                  user:user 
+//               })
           }catch(e){
                  res.status(400).json({
                   msg:e.message
